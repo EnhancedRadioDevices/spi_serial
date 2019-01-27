4632feacaa10794adad443a526fa502403642b85
morgan <morgan@enhancedradio.com>
2019-01-27 02:04:51 +0000

    bug fixes for neutis

--- spi_serial/spi_serial.py
+++ spi_serial/spi_serial.py
@@ -32,35 +32,37 @@ class SpiSerial():
         self.dev = spidev.SpiDev()           # create spi object
         self.dev.open(0, 0)                  # open spi port 0, device (CS) 0
         
-#        self.dev.frequency(62500)
-#        self.dev.mode(m.SPI_MODE0)
-#        self.dev.bitPerWord(8)
+        self.dev.max_speed_hz = (62500)
+        self.dev.mode = 0b00 #(m.SPI_MODE0)
+        self.dev.bits_per_word = (8)
+        self.dev.lsbfirst = True
         self.timeout = 0
         self.rx_buf = []
 
-    def spi_xfer(self, b):
-        tx = bytearray(1)
-        tx[0] = (int('{:08b}'.format(b)[::-1], 2))
-#        self.cs0.write(0)
-        rxbuf = self.dev.xfer(tx)
-#        self.cs0.write(1)
-        return (int('{:08b}'.format(rxbuf[0])[::-1], 2))
-
     def close(self):
-        pass
+        self.dev.close()
 
     def write(self, tx_bytes):
-        tx_bytes = bytearray(tx_bytes)
-        self.spi_xfer(0x99)
-        num_rxd = self.spi_xfer(len(tx_bytes))
-        for y in range(0, len(tx_bytes)):
-            rx = self.spi_xfer(tx_bytes[y])
-            if num_rxd > 0:
-                self.rx_buf.append(rx)
-                num_rxd -= 1
-        for y in range(0, num_rxd):
-            rx = self.spi_xfer(0)
-            self.rx_buf.append(rx)
+#        tx_bytes = bytearray(tx_bytes)
+        self.dev.xfer([0x99]) #spi_xfer(0x99)
+        num_rxd = self.dev.xfer([len(tx_bytes)]) #spi_xfer(len(tx_bytes))
+        num_rxd = num_rxd[0]
+        rx = self.dev.xfer(tx_bytes)
+        if (num_rxd > 0):
+            to_rx = min(num_rxd, len(rx))
+            self.rx_buf.extend(rx[:to_rx])
+            num_rxd = num_rxd - to_rx
+        if num_rxd > 0:
+            rx = self.dev.xfer([0]*num_rxd)
+            self.rx_buf.extend(rx)
+#        for y in range(0, len(tx_bytes)):
+#            rx = self.spi_xfer(tx_bytes[y])
+#            if num_rxd > 0:
+#                self.rx_buf.append(rx)
+#                num_rxd -= 1
+#        for y in range(0, num_rxd):
+#            rx = self.spi_xfer(0)
+#            self.rx_buf.append(rx)
 
     def read(self, num_bytes=0):
         if num_bytes == 0:
@@ -76,11 +78,15 @@ class SpiSerial():
         return self.read(1)
 
     def inWaiting(self):
-        self.spi_xfer(0x99)
-        num_rxd = self.spi_xfer(0)
-        for y in range(0, num_rxd):
-            rx = self.spi_xfer(0)
-            self.rx_buf.append(rx)
+        e = self.dev.xfer([0x99, 0x00])
+        #num_rxd = self.dev.xfer([0])
+        num_rxd = e[1]
+        if num_rxd > 0:
+            rx = self.dev.xfer([0]*num_rxd)
+            self.rx_buf.extend(rx)
+#        for y in range(0, num_rxd):
+#            rx = self.spi_xfer(0)
+#            self.rx_buf.append(rx)
         return len(self.rx_buf)
 
     def reset(self):
